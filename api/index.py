d409498ec2ed10a5c42756ecf49e8b4c275a0fc7
Aman Bhardwaj <whyamanbhardwaj@gmail.com>
2026-09-05 00:57:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../templates"
+)
 
-@app.route('/')
-def hello():
-    return render_template('home.html')
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    return render_template("home.html")
